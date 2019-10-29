8f2279ced88002264d445ee532c3d2fa9471c73d
Michael <mkang80@gmail.com>
2019-10-28 21:25:44 -0700

    Initial commit

---
+++ src/test/java/webDriverMaven/LoginTest.java
@@ -0,0 +1,38 @@
+package webDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("mkang80@gmail.com");
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
